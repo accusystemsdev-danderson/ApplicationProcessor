c36afcedf7ab29653996ced3b8892eb4802f0a45
amiller <amiller@cb216f19-f252-4bda-8bb7-1b62c1fd4a59>
2013-09-10 20:50:59 +0000

    Implemented AccuAccount.Data

--- AccuSystems/Products/Development/AccuLoan/Apps/ApplicationProcessor/Trunk/Code/ApplicationProcessor/MTEProcessor.cs
+++ AccuSystems/Products/Development/AccuLoan/Apps/ApplicationProcessor/Trunk/Code/ApplicationProcessor/MTEProcessor.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Xml;
 using System.Xml.Linq;
 using System.Text.RegularExpressions;
+using AccuAccount.Data;
 
 namespace ApplicationProcessor
 {
@@ -16,12 +17,12 @@ namespace ApplicationProcessor
         public DataTable TableToProcess { get; set; }
         public LogWriter LogFile { get; set; }
         public FieldMapper FieldMap { get; set; } 
-        private AcculoanDBEntities db { get; set; }
+        private DataContext db { get; set; }
 
         public bool processMTEs()
         {
+            db = new DataContext(Config.dbConnectionString);
             bool success = false;
-            db = new AcculoanDBEntities(Config.dbConnectionString);
             try
             {
                 if (!db.Database.Exists())
@@ -44,15 +45,15 @@ namespace ApplicationProcessor
                 string customerTaxID = row[FieldMap.taxIdFieldName].ToString();
 
                 //Check to see if any customers exist that share the same taxID, but under a different name
-                int DupTaxIDs = (from M in db.customers
-                                 where M.TaxId == customerTaxID && M.customerName.ToUpper() != customerName.ToUpper()
+                int DupTaxIDs = (from M in db.Customers
+                                 where M.TaxId == customerTaxID && M.CustomerName.ToUpper() != customerName.ToUpper()
                                  select M).Count();
 
                 if (DupTaxIDs > 0)
                 {
                     //get current entries in MTE_List
-                    var MTEs = from M in db.MTE_List
-                               where M.TaxID == customerTaxID
+                    var MTEs = from M in db.MTELists
+                               where M.TaxId == customerTaxID
                                select M;
                     
                     //if count = 0 then this taxID has not been an MTE before.  The current customer record in the customer table, as well
@@ -84,7 +85,7 @@ namespace ApplicationProcessor
                                     //appendages of '-' indicate an additional name for a single customer, replace the current name with the primary MTE record
                                     if (mteRecord.Appendage == "-")
                                     {
-                                        string primaryName = (from N in db.MTE_List
+                                        string primaryName = (from N in db.MTELists
                                                               where N.CustomerNumber == customerNumber && N.Appendage == "*"
                                                               select N.CustomerName).FirstOrDefault().ToString();
                                         row[FieldMap.customerNameFieldName] = primaryName;
@@ -99,7 +100,7 @@ namespace ApplicationProcessor
                                     //appendages with '-' followed by another single character indicate an additional name for a secondary customer, replace the current name and customer number
                                     else if (Regex.IsMatch(mteRecord.Appendage, "-[0-9a-zA-Z"))
                                     {
-                                        string primaryName = (from N in db.MTE_List
+                                        string primaryName = (from N in db.MTELists
                                                               where N.CustomerNumber == customerNumber && N.Appendage == mteRecord.Appendage.Replace("-", "")
                                                               select N.CustomerName).FirstOrDefault().ToString();
                                         row[FieldMap.customerNameFieldName] = primaryName;
@@ -133,21 +134,21 @@ namespace ApplicationProcessor
 
         private void addPrimaryRecordToMTEList(string taxID)
         {
-            var primaryRecord = (from c in db.customers
+            var primaryRecord = (from c in db.Customers
                                 where c.TaxId == taxID
                                  select c).FirstOrDefault();
-                addCurrentRecordToMTEList(primaryRecord.customerName, primaryRecord.customerNumber, primaryRecord.TaxId);
+                addCurrentRecordToMTEList(primaryRecord.CustomerName, primaryRecord.CustomerNumber, primaryRecord.TaxId);
         }
         
         private void addCurrentRecordToMTEList(string customerName, string customerNumber, string taxID)
         {
             {
-                MTE_List newMTERecord = new MTE_List();
-                newMTERecord.MTEID = Guid.NewGuid();
+                MTEList newMTERecord = new MTEList();
+                newMTERecord.MTEId = Guid.NewGuid();
                 newMTERecord.CustomerName = customerName;
                 newMTERecord.CustomerNumber = customerNumber;
-                newMTERecord.TaxID = taxID;
-                db.MTE_List.Add(newMTERecord);
+                newMTERecord.TaxId = taxID;
+                db.MTELists.Add(newMTERecord);
             }
         }
 
