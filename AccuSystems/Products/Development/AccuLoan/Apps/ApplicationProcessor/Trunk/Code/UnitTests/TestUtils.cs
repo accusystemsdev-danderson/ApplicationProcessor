1061269db329538c62a66c4dc814575e46b92be6
amiller <amiller@cb216f19-f252-4bda-8bb7-1b62c1fd4a59>
2013-11-26 17:27:10 +0000

    Added missing file TestUtils.cs

---
+++ AccuSystems/Products/Development/AccuLoan/Apps/ApplicationProcessor/Trunk/Code/UnitTests/TestUtils.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="TestUtils.cs" company="AccuSystems LLC">
+//     Copyright (c) AccuSystems.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------------
+
+
+namespace UnitTests
+{
+    using System.Xml.Linq;
+    using System.Xml;
+
+    public static class TestUtils
+    {
+        public static void SetupRuleXML(ApplicationProcessor.RuleDefinition rule)
+        {
+            XElement ruleXml = new XElement("ApplicationRules",
+                new XElement("Rule",
+                new XAttribute("Field", rule.Field),
+                new XAttribute("Operator", rule.Operator),
+                new XAttribute("Value", rule.Value),
+                new XAttribute("Action", rule.Action),
+                new XAttribute("Parameter1", rule.Parameter1),
+                new XAttribute("Parameter2", rule.Parameter2),
+                new XAttribute("Parameter3", rule.Parameter3)));
+
+            using (XmlWriter writer = XmlWriter.Create("testRules.xml"))
+            {
+                XDocument doc = new XDocument();
+                doc.Add(ruleXml);
+                doc.WriteTo(writer);
+                writer.Flush();
+            }
+        }
+
+        public static void SetupLogging()
+        {
+            ApplicationProcessor.LogWriter.LogFilePath = "logs\\";
+            ApplicationProcessor.LogWriter.LogFileName = "UnitTestLog.txt";
+
+            ApplicationProcessor.LogWriter.OpenLog();
+        }
+    }
+}
