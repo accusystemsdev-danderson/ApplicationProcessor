48e0bad72632fed584e3e620a1194ffa58d53775
amiller <amiller@cb216f19-f252-4bda-8bb7-1b62c1fd4a59>
2014-08-21 00:31:06 +0000

    Errors in MTE logic. Added trim when combining fields with space

--- AccuSystems/Products/Development/AccuLoan/Apps/ApplicationProcessor/Trunk/Code/ApplicationProcessor/MTEProcessor.cs
+++ AccuSystems/Products/Development/AccuLoan/Apps/ApplicationProcessor/Trunk/Code/ApplicationProcessor/MTEProcessor.cs
@@ -8,31 +8,26 @@ namespace ApplicationProcessor
 {
     using AccuAccount.Data;
     using System;
-    using System.Collections.Generic;
-    using System.Data;
     using System.Linq;
-    using System.Text;
     using System.Text.RegularExpressions;
-    using System.Threading.Tasks;
-    using System.Xml;
-    using System.Xml.Linq;
+    
 
     /// <summary>
     /// Processes MTE information for customers in a DataTable
     /// </summary>
-    public static class MTEProcessor
+    public static class MteProcessor
     {
-        private static DataContext db = new DataContext();
+        private static readonly DataContext DB = new DataContext();
 
         /// <summary>
         /// Updates CustomerName or CustomerNumber if MTE records indicate an update should occur
         /// </summary>
         /// <returns>True upon successful processing</returns>
-        public static bool ProcessMTEs(SourceRecord record)
+        public static bool ProcessMte(SourceRecord record)
         {
             try
             {
-                if (!db.Database.Exists())
+                if (!DB.Database.Exists())
                 {
                     LogWriter.LogMessage("Unable to connect to database");
                     return false;
@@ -45,128 +40,182 @@ namespace ApplicationProcessor
                 LogWriter.LogMessage("Unable to connect to database");
                 return false;
             }
-            
-            int DupTaxIDs = (from M in db.Customers
-                             where M.TaxId.Equals(record.TaxId, StringComparison.OrdinalIgnoreCase) && 
-                                !M.CustomerName.Equals(record.CustomerName, StringComparison.OrdinalIgnoreCase)
-                             select M).Count();
 
-            if (DupTaxIDs > 0)
+            bool duplicateTaxIDs =
+                DB.Customers.Any(c => c.TaxId.Equals(record.TaxId, StringComparison.OrdinalIgnoreCase) &&
+                                        !c.CustomerName.Equals(record.CustomerName, StringComparison.OrdinalIgnoreCase));
+
+            bool existingMtes = DB.MTELists.Any(p => p.TaxId.Equals(record.TaxId, StringComparison.OrdinalIgnoreCase));
+
+            if (duplicateTaxIDs || existingMtes)
             {
-                UpdateRecordFromMteList(record);
+                if (!UpdateRecordFromMteList(record))
+                {
+                    //todo: default processing for unresolved mtes.
+
+                    record.IgnoreRecord = true;
+                    LogWriter.LogMessage(string.Format("Ignoring record - MTE needs to be resolved.  Application Number: {0} TaxId: {1} Customer Name: {2}",
+                                                       record.LoanNumber,
+                                                       record.TaxId,
+                                                       record.CustomerName));
+                    return false;
+                }
             }
             return true;
         }
-        
+
         /// <summary>
         /// Reads MTE records from MTE_List and updates record with correct CustomerName and CustomerNumber
         /// </summary>
         /// <param name="record">The source record to process</param>
-        private static void UpdateRecordFromMteList(SourceRecord record)
+        /// <returns>True if the record was updated from an mte record.  False if the mte is unresolved</returns>
+        private static bool UpdateRecordFromMteList(SourceRecord record)
         {
-            var MTEs = from M in db.MTELists
-                       where M.TaxId.Equals(record.TaxId, StringComparison.OrdinalIgnoreCase)
-                       select M;
-                    
+            var mtes = DB.MTELists.Where(m => m.TaxId.Equals(record.TaxId, StringComparison.OrdinalIgnoreCase));
+
             //if count = 0 then this taxID has not been an MTE before.  The current customer record in the customer table, as well
             //as the record being processed must be added to the table.
-            if (MTEs.Count() == 0)
+            if (!mtes.Any())
             {
-                AddPrimaryRecordToMTEList(record.TaxId);
-                AddCurrentRecordToMTEList(record.CustomerName, record.CustomerNumber, record.TaxId);
+                AddPrimaryRecordToMteList(record.TaxId);
+                AddNewMteListRecord(record.CustomerName, record.CustomerNumber, record.TaxId, string.Empty);
+                return false;
             }
 
-            else
+            var matchedMteRecord = mtes.SingleOrDefault(m => m.CustomerName.Equals(record.CustomerName, StringComparison.OrdinalIgnoreCase));
+
+            if (matchedMteRecord == null)
             {
-                bool currentRecordExits = false;
-
-                foreach (var mteRecord in MTEs)
-                {
-
-                    if (mteRecord.CustomerName.Equals(record.CustomerName, StringComparison.OrdinalIgnoreCase))
-                    {
-                        currentRecordExits = true;
-
-                        if (mteRecord.Appendage != "*" && mteRecord.Appendage != null)
-                        {
-                            //appendages of '-' indicate an additional name for a single customer, replace the current name with the primary MTE record
-                            if (mteRecord.Appendage == "-")
-                            {
-                                string primaryName = (from N in db.MTELists
-                                                      where N.CustomerNumber == record.CustomerNumber && N.Appendage == "*"
-                                                      select N.CustomerName).FirstOrDefault().ToString();
-                                record.CustomerName = primaryName;
-                            }
-
-                            //appendages of any other single character indicate an additional customer, append the character to the customer number
-                            else if (Regex.IsMatch(mteRecord.Appendage, "[0-9a-zA-Z]"))
-                            {
-                                record.CustomerNumber = record.CustomerNumber + "-" + mteRecord.Appendage;
-                            }
-
-                            //appendages with '-' followed by another single character indicate an additional name for a secondary customer, replace the current name and customer number
-                            else if (Regex.IsMatch(mteRecord.Appendage, "-[0-9a-zA-Z]"))
-                            {
-                                string primaryName = (from N in db.MTELists
-                                                      where N.CustomerNumber == record.CustomerNumber && N.Appendage == mteRecord.Appendage.Replace("-", "")
-                                                      select N.CustomerName).FirstOrDefault().ToString();
-                                record.CustomerName = primaryName;
-                                record.CustomerNumber = record.CustomerNumber + "-" + mteRecord.Appendage;
-                            }
-                        }
-                    }
-                }
-
-                if (!currentRecordExits)
-                {
-                    AddCurrentRecordToMTEList(record.CustomerName, record.CustomerNumber, record.TaxId);
-                }
+                AddNewMteListRecord(record.CustomerName, record.CustomerNumber, record.TaxId, string.Empty);
+                return false;
             }
 
+            if (matchedMteRecord.Appendage == "*")
+            {
+                return true;
+            }
+
+            if (matchedMteRecord.Appendage == "-")
+            {
+                //appendages of '-' indicate an additional name for a single customer, replace the current name with the primary MTE record
+                var primaryMte =
+                    DB.MTELists.SingleOrDefault(m => m.TaxId == record.TaxId &&
+                                                    m.Appendage == "*");
+
+                if (primaryMte != null)
+                {
+                    record.CustomerName = primaryMte.CustomerName;
+                    return true;
+                }
                 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                LogWriter.LogMessage(e.ToString());
-                LogWriter.LogMessage();
-                LogWriter.LogMessage("Unable to save to MTE table");
-
+                return false;
             }
 
+            if (Regex.IsMatch(matchedMteRecord.Appendage, "^[0-9|a-z|A-Z]"))
+            {
+                //appendages of any other single character indicate an additional customer, append the character to the customer number
+                record.CustomerNumber = string.Format("{0}-{1}",
+                                                      record.CustomerNumber,
+                                                      matchedMteRecord.Appendage);
+
+                return true;
+            }
+
+            if (Regex.IsMatch(matchedMteRecord.Appendage, "-[0-9|a-z|A-Z]"))
+            {
+                //appendages with '-' followed by another single character indicate an additional name for a secondary customer, replace the current name and customer number
+                var primaryMte =
+                    DB.MTELists.SingleOrDefault(m => m.TaxId == record.TaxId &&
+                                                     m.Appendage == matchedMteRecord.Appendage.Replace("-", ""));
+
+                if (primaryMte != null)
+                {
+                    record.CustomerName   = primaryMte.CustomerName;
+                    record.CustomerNumber = string.Format("{0}-{1}", record.CustomerNumber, primaryMte.Appendage);
+                    return true;
+                }
+                return false;
+            }
+
+            if (matchedMteRecord.Appendage == "~")
+            {
+                //appendages of '~' are a second name line.  Replace the current name with the primary record, and assign the current name as name line 2
+                var primaryMte =
+                    DB.MTELists.SingleOrDefault(m => m.TaxId == record.TaxId &&
+                                                     m.Appendage == "*");
+
+                if (primaryMte != null)
+                {
+                    record.BusinessName = record.CustomerName;
+                    record.CustomerName = primaryMte.CustomerName;
+                    return true;
+                }
+                return false;
+            }
+
+            if (Regex.IsMatch(matchedMteRecord.Appendage, "~[0-9a-zA-Z]"))
+            {
+                //apendages of '~' followed by another single character are a second name line for an additional customer, replace the current name with the primary record and assign
+                //the current name as name line 2
+                var primaryMte =
+                    DB.MTELists.SingleOrDefault(m => m.TaxId == record.TaxId &&
+                                                     m.Appendage == matchedMteRecord.Appendage.Replace("~", ""));
+
+                if (primaryMte != null)
+                {
+                    record.BusinessName = record.CustomerName;
+                    record.CustomerName = primaryMte.CustomerName;
+                    record.CustomerNumber = string.Format("{0}-{1}", record.CustomerNumber, primaryMte.Appendage);
+                    return true;
+                }
+                return false;
+            }
+
+            return false;
         }
 
         /// <summary>
         /// Creates an MTE record for the primary holder of a taxId
         /// </summary>
         /// <param name="taxID">The taxId to lookup</param>
-        private static void AddPrimaryRecordToMTEList(string taxID)
+        private static void AddPrimaryRecordToMteList(string taxID)
         {
-            var primaryRecord = (from c in db.Customers
-                                where c.TaxId == taxID
-                                 select c).FirstOrDefault();
-                AddCurrentRecordToMTEList(primaryRecord.CustomerName, primaryRecord.CustomerNumber, primaryRecord.TaxId);
+            var primaryRecord = DB.Customers.First(c => c.TaxId == taxID);
+
+            AddNewMteListRecord(primaryRecord.CustomerName, primaryRecord.CustomerNumber, primaryRecord.TaxId, string.Empty);
         }
-        
+
         /// <summary>
         /// creates an unresolved MTE record for a customer
         /// </summary>
         /// <param name="customerName">The Customer Name for the MTE</param>
         /// <param name="customerNumber">The Customer Number for the MTE</param>
         /// <param name="taxID">The TaxId for the MTE</param>
-        private static void AddCurrentRecordToMTEList(string customerName, string customerNumber, string taxID)
+        /// <param name="appendage">The appendage for the MTE record</param>
+        private static void AddNewMteListRecord(string customerName, string customerNumber, string taxID, string appendage)
         {
+
+            var newMteRecord = new MTEList
+                               {
+                                   MTEId = Guid.NewGuid(),
+                                   CustomerName = customerName,
+                                   CustomerNumber = customerNumber,
+                                   TaxId = taxID,
+                                   Appendage = appendage
+                               };
+
+            DB.MTELists.Add(newMteRecord);
+
+            try
             {
-                MTEList newMTERecord = new MTEList();
-                newMTERecord.MTEId = Guid.NewGuid();
-                newMTERecord.CustomerName = customerName;
-                newMTERecord.CustomerNumber = customerNumber;
-                newMTERecord.TaxId = taxID;
-                db.MTELists.Add(newMTERecord);
+                DB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                LogWriter.LogMessage(e.ToString());
+                LogWriter.LogMessage();
+                LogWriter.LogMessage("Unable to save to MTE table");
             }
         }
-
     }
 }
